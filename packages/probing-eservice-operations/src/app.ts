bb9a307ac8413a4b083c427f73c4d242a37db07c
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-01-22 14:48:15 +0100

    Updarte express app with zodiosCtx

--- packages/probing-eservice-operations/src/app.ts
+++ packages/probing-eservice-operations/src/app.ts
@@ -1,14 +1,18 @@
-import express, { Application } from "express";
+import { zodiosCtx } from "pagopa-interop-probing-commons";
 import * as AWS_SDK from "aws-sdk";
 import * as AWSXRay from "aws-xray-sdk";
 import * as https from "https";
 import eServiceRouter from "./routers/eserviceRouter.js";
 
-const app: Application = express();
+const app = zodiosCtx.app();
+
+// Disable the "X-Powered-By: Express" HTTP header for security reasons.
+// See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
+app.disable("x-powered-by");
 
 // Capture all AWS clients we create
 const AWS = AWSXRay.captureAWS(AWS_SDK);
-AWS.config.update({region: process.env.DEFAULT_AWS_REGION || 'us-west-2'});
+AWS.config.update({ region: process.env.DEFAULT_AWS_REGION || "us-west-2" });
 
 // Capture all outgoing https requests
 AWSXRay.captureHTTPsGlobal(https);
@@ -17,11 +21,7 @@ AWSXRay.captureHTTPsGlobal(https);
 const XRayExpress = AWSXRay.express;
 app.use(XRayExpress.openSegment("pagopa-probing-eservice-operations"));
 
-// Disable the "X-Powered-By: Express" HTTP header for security reasons.
-// See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
-app.disable("x-powered-by");
-
-app.use(eServiceRouter);
+app.use(eServiceRouter(zodiosCtx));
 
 app.use(XRayExpress.closeSegment());
 
