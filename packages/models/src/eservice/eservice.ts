4eff13a44e892e47727651430384cd1ab0f00cf0
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-01-30 19:03:08 +0100

    Update eservice models

--- packages/models/src/eservice/eservice.ts
+++ packages/models/src/eservice/eservice.ts
@@ -54,7 +54,9 @@ export type ChangeEserviceStateRequest = z.infer<
 >;
 
 export const ChangeProbingFrequencyRequest = z.object({
-  pollingFrequency: z.number().int().gte(1).default(5),
+  pollingFrequency: z.number().int().gte(1).default(5).optional(),
+  pollingStartTime: z.string(),
+  pollingEndTime: z.string(),
 });
 
 export type ChangeProbingFrequencyRequest = z.infer<
@@ -62,9 +64,11 @@ export type ChangeProbingFrequencyRequest = z.infer<
 >;
 
 export const EserviceProbingUpdateLastRequest = z.object({
-  lastRequest: z.string().datetime({ offset: true })
+  lastRequest: z.string().datetime({ offset: true }),
 });
-export type EserviceProbingUpdateLastRequest = z.infer<typeof EserviceProbingUpdateLastRequest>;
+export type EserviceProbingUpdateLastRequest = z.infer<
+  typeof EserviceProbingUpdateLastRequest
+>;
 
 export const ChangeResponseReceived = z.object({
   responseReceived: z.string().datetime({ offset: true }),
@@ -84,33 +88,39 @@ export const EserviceSaveRequest = z.object({
 export type EserviceSaveRequest = z.infer<typeof EserviceSaveRequest>;
 
 export const EService = z.object({
-  eserviceRecordId: z.number().int(),
+  eserviceRecordId: z.string().transform((value) => Number(value)),
+  // In PostgreSQL, the primary key column type for eserviceRecordId is bigint, which returns a string.
+  // Due to the JavaScript limit for accurate representation of integers being 9007199254740991 (2^53),
+  // values exceeding this limit must returned as strings.
+  // Despite this, the API Swagger documentation specifies a number type.
+  // We transform it to a number with the understanding that eservices' numbers are assumed
+  // to stay within the safe JavaScript integer limit.
+  // For more details, refer to: https://github.com/typeorm/typeorm/issues/8583
   eserviceId: z.string().uuid(),
   versionId: z.string().uuid(),
   eserviceName: z.string(),
   producerName: z.string(),
   state: EserviceInteropState,
-  responseReceived: z.string().datetime({ offset: true }),
-  lastRequest: z.string().datetime({ offset: true }),
-  responseStatus: EserviceStatus,
   versionNumber: z.number().int(),
   basePath: z.array(z.string()),
   technology: EserviceTechnology,
-  pollingFrequency: z.number().int().gte(1).default(5).optional(),
+  pollingFrequency: z.number().int().min(1).default(5).optional(),
   probingEnabled: z.boolean(),
   audience: z.array(z.string()),
   pollingStartTime: z.string(),
   pollingEndTime: z.string(),
+  lockVersion: z.number(),
 });
 export type EService = z.infer<typeof EService>;
 
 export const EServiceContent = z.object({
-  eserviceRecordId: z.number().int(),
+  eserviceRecordId: z.string().transform((value) => Number(value)),
+  // For more details, refer to: EService schema declaration
   eserviceName: z.string(),
   producerName: z.string(),
   state: EserviceInteropState,
-  responseReceived: z.string().datetime({ offset: true }),
-  lastRequest: z.string().datetime({ offset: true }),
+  responseReceived: z.date().transform((date) => date.toISOString()),
+  lastRequest: z.date().transform((date) => date.toISOString()),
   responseStatus: EserviceStatus,
   versionNumber: z.number().int(),
   basePath: z.array(z.string()),
@@ -134,8 +144,8 @@ export type EServiceMainData = z.infer<typeof EServiceMainData>;
 export const EServiceProbingData = z.object({
   probingEnabled: z.boolean(),
   state: EserviceInteropState,
-  responseReceived: z.string().datetime({ offset: true }),
-  lastRequest: z.string().datetime({ offset: true }),
+  responseReceived: z.date().transform((date) => date.toISOString()),
+  lastRequest: z.date().transform((date) => date.toISOString()),
   responseStatus: EserviceStatus,
   pollingFrequency: z.number().int(),
 });
