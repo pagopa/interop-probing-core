73e7116c75525caa0f81e916bc4789f41c0a7faf
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-07 14:56:45 +0100

    Refactor: Added processExit function to handle process termination on consumer error, separated initialization consumer  into initConsumer function, and improved logging

--- packages/commons/src/sqs/sqs.ts
+++ packages/commons/src/sqs/sqs.ts
@@ -6,12 +6,19 @@ import {
   Message,
 } from "@aws-sdk/client-sqs";
 import pkg from "aws-xray-sdk";
+import { logger } from "../logging/index.js";
+import { ConsumerConfig } from "../index.js";
 
 interface SQSConfig {
   awsRegion: string;
 }
 
 const { captureAWSv3Client, Segment } = pkg;
+
+const processExit = (existStatusCode: number = 1): void => {
+  logger.info(`Process exit with code ${existStatusCode}`);
+  process.exit(existStatusCode);
+};
 
 export const instantiateSQSClient = (
   config: SQSConfig,
@@ -25,33 +32,57 @@ export const instantiateSQSClient = (
   return sqsClient;
 };
 
-export const sqsRunConsumer = async (
+const initConsumer = async (
   sqsClient: SQSClient,
   queueUrl: string,
   consumerHandler: (messagePayload: Message) => Promise<void>
 ): Promise<void> => {
+  logger.info(`Consumer connecting to queue: ${queueUrl}`);
+
   const command = new ReceiveMessageCommand({
     QueueUrl: queueUrl,
     MaxNumberOfMessages: 10,
   });
 
   const { Messages } = await sqsClient.send(command);
-  if (Messages?.length) {
-    try {
-      for (const message of Messages) {
-        if (!message.ReceiptHandle) {
-          throw new Error(
-            `ReceiptHandle not found in Message: ${JSON.stringify(message)}`
-          );
-        }
 
-        await consumerHandler(message);
-        await sqsDeleteMessage(sqsClient, queueUrl, message.ReceiptHandle);
+  logger.info(
+    `Consumer successfully connected to and received messages from queue: ${queueUrl}`
+  );
+
+  if (Messages?.length) {
+    for (const message of Messages) {
+      if (!message.ReceiptHandle) {
+        throw new Error(
+          `ReceiptHandle not found in Message: ${JSON.stringify(message)}`
+        );
       }
-    } catch (e) {
-      throw new Error(`Unexpected error on Consumer queue: ${e}`);
+
+      await consumerHandler(message);
+      await sqsDeleteMessage(sqsClient, queueUrl, message.ReceiptHandle);
     }
   }
+};
+
+export const sqsRunConsumer = async (
+  sqsClient: SQSClient,
+  config: { queueUrl: string } & ConsumerConfig,
+  consumerHandler: (messagePayload: Message) => Promise<void>
+): Promise<void> => {
+  do {
+    try {
+      await initConsumer(sqsClient, config.queueUrl, consumerHandler);
+
+      await new Promise((resolve) =>
+        setTimeout(resolve, config.defaultConsumerTimeout * 1000)
+      );
+    } catch (e) {
+      logger.error(
+        `Generic error occurs during consumer process. Details: ${e}`
+      );
+      processExit();
+    }
+  } while (true);
 };
 
 export const sqsSendMessage = async (
