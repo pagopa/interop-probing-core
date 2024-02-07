f66cb361e6e2a81cef989b25e8a8890d8bec08d5
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-07 14:58:37 +0100

    Update config probing-response-updater

--- packages/probing-response-updater/src/utilities/config.ts
+++ packages/probing-response-updater/src/utilities/config.ts
@@ -1,19 +1,15 @@
-import { AWSConfig } from "pagopa-interop-probing-commons";
+import { AWSConfig, ConsumerConfig } from "pagopa-interop-probing-commons";
 import { z } from "zod";
 
-const eServiceResponseUpdaterConfig = AWSConfig.and(
+const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig).and(
   z
     .object({
       INTEROP_PROBING_RESPONSE_UPDATER_NAME: z.string(),
       SQS_ENDPOINT_POLL_RESULT_QUEUE: z.string(),
-      API_OPERATIONS_BASEURL: z.string(),
-      API_OPERATIONS_ESERVICE_BASEPATH: z.string(),
     })
     .transform((c) => ({
       applicationName: c.INTEROP_PROBING_RESPONSE_UPDATER_NAME,
       sqsEndpointPollResultQueue: c.SQS_ENDPOINT_POLL_RESULT_QUEUE,
-      apiOperationsBaseUrl: c.API_OPERATIONS_BASEURL,
-      apiOperationsEserviceBasePath: c.API_OPERATIONS_ESERVICE_BASEPATH,
     }))
 );
 
