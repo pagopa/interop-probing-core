a104ce3b0c148fea9f971e46bc139905c9bafad9
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-15 19:11:36 +0100

    Handling configurations as separated exports

---
+++ packages/commons/src/config/index.ts
@@ -0,0 +1,3 @@
+export { AWSConfig } from "./awsConfig.js";
+export { consumerConfig, ConsumerConfig } from "./consumerConfig.js";
+export { httpServerConfig, HTTPServerConfig } from "./httpServerConfig.js";
--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -3,6 +3,4 @@ export * from "./logging/index.js";
 export * from "./model/apiEndpoint.js";
 export * from "./types/index.js";
 export * from "./sqs/index.js";
-export * from "./config/commonConfig.js";
-export * from "./config/awsConfig.js";
-export * from "./config/consumerConfig.js";
+export * from "./config/index.js";
