028f38d1c338a5d25466a46da0eff3a830804889
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-16 11:39:49 +0100

    export LoggerConfig commons

--- packages/commons/src/config/index.ts
+++ packages/commons/src/config/index.ts
@@ -1,3 +1,4 @@
 export { AWSConfig } from "./awsConfig.js";
 export { consumerConfig, ConsumerConfig } from "./consumerConfig.js";
 export { httpServerConfig, HTTPServerConfig } from "./httpServerConfig.js";
+export { loggerConfig, LoggerConfig } from "./loggerConfig.js";
