5a6c0d6b39928ccadacdf9f3585abe66268974fb
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-16 11:54:07 +0100

    added AWSConfig config eservice-operations

--- packages/probing-eservice-operations/src/utilities/config.ts
+++ packages/probing-eservice-operations/src/utilities/config.ts
@@ -1,20 +1,25 @@
-import { HTTPServerConfig, LoggerConfig } from "pagopa-interop-probing-commons";
+import {
+  HTTPServerConfig,
+  LoggerConfig,
+  AWSConfig,
+} from "pagopa-interop-probing-commons";
 import { DbConfig } from "./dbConfig.js";
 import { z } from "zod";
 
-const eServiceOperationsConfig = HTTPServerConfig.and(LoggerConfig).and(DbConfig).and(
-  z
-    .object({
-      TOLERANCE_MULTIPLIER_IN_MINUTES: z.coerce.number().min(1),
-      SCHEMA_NAME: z.string(),
-      AWS_REGION: z.string(),
-    })
-    .transform((c) => ({
-      minOfTolleranceMultiplier: c.TOLERANCE_MULTIPLIER_IN_MINUTES,
-      schemaName: c.SCHEMA_NAME,
-      awsRegion: c.AWS_REGION
-    }))
-);
+const eServiceOperationsConfig = HTTPServerConfig.and(LoggerConfig)
+  .and(AWSConfig)
+  .and(DbConfig)
+  .and(
+    z
+      .object({
+        TOLERANCE_MULTIPLIER_IN_MINUTES: z.coerce.number().min(1),
+        SCHEMA_NAME: z.string(),
+      })
+      .transform((c) => ({
+        minOfTolleranceMultiplier: c.TOLERANCE_MULTIPLIER_IN_MINUTES,
+        schemaName: c.SCHEMA_NAME,
+      }))
+  );
 
 export type EServiceOperationsConfig = z.infer<typeof eServiceOperationsConfig>;
 
