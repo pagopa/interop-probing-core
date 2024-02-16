780c08ca1f9e16d4f2fefbb270841936621bc762
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-16 11:52:44 +0100

    Added AWSConfig config probing-api

--- packages/probing-api/src/utilities/config.ts
+++ packages/probing-api/src/utilities/config.ts
@@ -1,19 +1,23 @@
-import { HTTPServerConfig, LoggerConfig } from "pagopa-interop-probing-commons";
+import {
+  HTTPServerConfig,
+  LoggerConfig,
+  AWSConfig,
+} from "pagopa-interop-probing-commons";
 import { z } from "zod";
 
-const apiConfig = HTTPServerConfig.and(LoggerConfig).and(
-  z
-    .object({
-      AWS_REGION: z.string(),
-      API_OPERATIONS_BASEURL: z.string(),
-      TOLERANCE_MULTIPLIER_IN_MINUTES: z.coerce.number().min(1),
-    })
-    .transform((c) => ({
-      awsRegion: c.AWS_REGION,
-      operationsBaseUrl: c.API_OPERATIONS_BASEURL,
-      minOfTolleranceMultiplier: c.TOLERANCE_MULTIPLIER_IN_MINUTES,
-    }))
-);
+const apiConfig = HTTPServerConfig.and(LoggerConfig)
+  .and(AWSConfig)
+  .and(
+    z
+      .object({
+        API_OPERATIONS_BASEURL: z.string(),
+        TOLERANCE_MULTIPLIER_IN_MINUTES: z.coerce.number().min(1),
+      })
+      .transform((c) => ({
+        operationsBaseUrl: c.API_OPERATIONS_BASEURL,
+        minOfTolleranceMultiplier: c.TOLERANCE_MULTIPLIER_IN_MINUTES,
+      }))
+  );
 
 export type ApiConfig = z.infer<typeof apiConfig>;
 
