19d20a9bc7542eccf7d552d16324e347cb09e312
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-16 11:49:12 +0100

    Fix config response-updater

--- packages/probing-response-updater/src/utilities/config.ts
+++ packages/probing-response-updater/src/utilities/config.ts
@@ -1,19 +1,25 @@
-import { AWSConfig, ConsumerConfig } from "pagopa-interop-probing-commons";
+import {
+  AWSConfig,
+  ConsumerConfig,
+  LoggerConfig,
+} from "pagopa-interop-probing-commons";
 import { z } from "zod";
 
-const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig).and(
-  z
-    .object({
-      INTEROP_PROBING_RESPONSE_UPDATER_NAME: z.string(),
-      SQS_ENDPOINT_POLL_RESULT_QUEUE: z.string(),
-      API_OPERATIONS_BASEURL: z.string(),
-    })
-    .transform((c) => ({
-      applicationName: c.INTEROP_PROBING_RESPONSE_UPDATER_NAME,
-      sqsEndpointPollResultQueue: c.SQS_ENDPOINT_POLL_RESULT_QUEUE,
-      operationsBaseUrl: c.API_OPERATIONS_BASEURL,
-    }))
-);
+const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig)
+  .and(LoggerConfig)
+  .and(
+    z
+      .object({
+        INTEROP_PROBING_RESPONSE_UPDATER_NAME: z.string(),
+        SQS_ENDPOINT_POLL_RESULT_QUEUE: z.string(),
+        API_OPERATIONS_BASEURL: z.string(),
+      })
+      .transform((c) => ({
+        applicationName: c.INTEROP_PROBING_RESPONSE_UPDATER_NAME,
+        sqsEndpointPollResultQueue: c.SQS_ENDPOINT_POLL_RESULT_QUEUE,
+        operationsBaseUrl: c.API_OPERATIONS_BASEURL,
+      }))
+  );
 
 export type EServiceResponseUpdaterConfig = z.infer<
   typeof eServiceResponseUpdaterConfig
