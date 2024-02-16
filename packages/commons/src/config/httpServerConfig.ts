64087b4ecf010049e04be4bf3fb43b92c2f1fee4
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-16 08:08:33 +0100

    Handling configurations as separated exports

---
+++ packages/commons/src/config/httpServerConfig.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { APIEndpoint } from "../model/apiEndpoint.js";
+
+export const HTTPServerConfig = z
+  .object({
+    HOST: APIEndpoint,
+    PORT: z.coerce.number().min(1001),
+  })
+  .transform((c) => ({
+    host: c.HOST,
+    port: c.PORT,
+  }));
+export type HTTPServerConfig = z.infer<typeof HTTPServerConfig>;
+
+export type CommonConfig = z.infer<typeof HTTPServerConfig>;
+
+export const httpServerConfig: () => HTTPServerConfig = () =>
+HTTPServerConfig.parse(process.env);
