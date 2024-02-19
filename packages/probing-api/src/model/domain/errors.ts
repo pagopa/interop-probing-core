607055a261e50f327b63ee1b47ff361dc218a808
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-19 18:35:50 +0100

    Handling operations client errors

--- packages/probing-api/src/model/domain/errors.ts
+++ packages/probing-api/src/model/domain/errors.ts
@@ -1,40 +1,16 @@
-import { ApiError, makeApiProblemBuilder } from "pagopa-interop-probing-models";
+import { makeApiProblemBuilder, Problem } from "pagopa-interop-probing-models";
+import { AxiosError } from "axios";
 
-export const errorCodes = {
-  eServiceNotFound: "0001",
+export const makeApiProblem = makeApiProblemBuilder({});
+
+export const resolveOperationsApiClientProblem = (error: unknown): Problem => {
+  const operationsApiProblem = Problem.safeParse(
+    (error as AxiosError).response?.data
+  );
+
+  if (operationsApiProblem.success) {
+    return operationsApiProblem.data;
+  } else {
+    return makeApiProblem(error, () => 500);
+  }
 };
-
-export type ErrorCodes = keyof typeof errorCodes;
-
-export const makeApiProblem = makeApiProblemBuilder(errorCodes);
-
-export function eServiceNotFound(
-  eserviceId: string,
-  versionId: string
-): ApiError<ErrorCodes> {
-  return new ApiError({
-    detail: `EService by ${eserviceId} version ${versionId} not found`,
-    code: "eServiceNotFound",
-    title: "EService not found",
-  });
-}
-
-export function eServiceMainDataByRecordIdNotFound(
-  eserviceRecordId: number
-): ApiError<ErrorCodes> {
-  return new ApiError({
-    detail: `EService main data by eserviceRecordId ${eserviceRecordId} not found`,
-    code: "eServiceNotFound",
-    title: "EService not found",
-  });
-}
-
-export function eServiceProbingDataByRecordIdNotFound(
-  eserviceRecordId: number
-): ApiError<ErrorCodes> {
-  return new ApiError({
-    detail: `EService probing data by eserviceRecordId ${eserviceRecordId} not found`,
-    code: "eServiceNotFound",
-    title: "EService not found",
-  });
-}
--- packages/probing-api/src/routers/eserviceRouter.ts
+++ packages/probing-api/src/routers/eserviceRouter.ts
@@ -1,147 +1,148 @@
-import { ZodiosEndpointDefinitions } from "@zodios/core";
+import { ZodiosEndpointDefinitions, ZodiosInstance } from "@zodios/core";
 import { ZodiosRouter } from "@zodios/express";
-import { makeApiProblem } from "../model/domain/errors.js";
+import { resolveOperationsApiClientProblem } from "../model/domain/errors.js";
 import { ExpressContext, ZodiosContext } from "pagopa-interop-probing-commons";
-import { config } from "../utilities/config.js";
 import {
   OperationsService,
   operationsServiceBuilder,
-} from "../services/eserviceService.js";
+} from "../services/operationsService.js";
 import { api } from "../model/generated/api.js";
-import { updateEServiceErrorMapper } from "../utilities/errorMappers.js";
-import { createApiClient as createOperationsApiClient } from "../../../probing-eservice-operations/src/model/generated/api.js";
-
-const operationsApiClient = createOperationsApiClient(config.operationsBaseUrl);
-const operationsService: OperationsService =
-  operationsServiceBuilder(operationsApiClient);
+import { Api } from "../../../probing-eservice-operations/src/model/types.js";
 
 const eServiceRouter = (
   ctx: ZodiosContext
-): ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext> => {
-  const eServiceRouter = ctx.router(api.api);
+): ((
+  operationsApiClient: ZodiosInstance<Api>
+) => ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext>) => {
+  return (operationsApiClient: ZodiosInstance<Api>) => {
+    const operationsService: OperationsService =
+      operationsServiceBuilder(operationsApiClient);
+    const router = ctx.router(api.api);
 
-  eServiceRouter
-    .post(
-      "/eservices/:eserviceId/versions/:versionId/updateState",
-      async (req, res) => {
+    router
+      .post(
+        "/eservices/:eserviceId/versions/:versionId/updateState",
+        async (req, res) => {
+          try {
+            await operationsService.updateEserviceState(
+              req.params.eserviceId,
+              req.params.versionId,
+              req.body
+            );
+            return res.status(204).end();
+          } catch (error) {
+            const errorRes = resolveOperationsApiClientProblem(error);
+            return res.status(errorRes.status).json(errorRes).end();
+          }
+        }
+      )
+      .post(
+        "/eservices/:eserviceId/versions/:versionId/probing/updateState",
+        async (req, res) => {
+          try {
+            await operationsService.updateEserviceProbingState(
+              req.params.eserviceId,
+              req.params.versionId,
+              req.body
+            );
+            return res.status(204).end();
+          } catch (error) {
+            const errorRes = resolveOperationsApiClientProblem(error);
+            return res.status(errorRes.status).json(errorRes).end();
+          }
+        }
+      )
+      .post(
+        "/eservices/:eserviceId/versions/:versionId/updateFrequency",
+        async (req, res) => {
+          try {
+            await operationsService.updateEserviceFrequency(
+              req.params.eserviceId,
+              req.params.versionId,
+              req.body
+            );
+            return res.status(204).end();
+          } catch (error) {
+            const errorRes = resolveOperationsApiClientProblem(error);
+            return res.status(errorRes.status).json(errorRes).end();
+          }
+        }
+      );
+
+    router
+      .get("/eservices", async (req, res) => {
         try {
-          await operationsService.updateEserviceState(
-            req.params.eserviceId,
-            req.params.versionId,
-            req.body
+          const { eserviceName, producerName, state, versionNumber } =
+            req.query;
+          const eservices = await operationsService.getEservices(
+            {
+              ...(eserviceName && { eserviceName }),
+              ...(producerName && { producerName }),
+              ...(state && { state }),
+              ...(versionNumber && { versionNumber: Number(versionNumber) }),
+            },
+            req.query.limit,
+            req.query.offset
           );
-          return res.status(204).end();
+
+          return res
+            .status(200)
+            .json({
+              content: eservices.content,
+              offset: eservices.offset,
+              limit: eservices.limit,
+              totalElements: eservices.totalElements,
+            })
+            .end();
         } catch (error) {
-          const errorRes = makeApiProblem(error, updateEServiceErrorMapper);
+          const errorRes = resolveOperationsApiClientProblem(error);
           return res.status(errorRes.status).json(errorRes).end();
         }
-      }
-    )
-    .post(
-      "/eservices/:eserviceId/versions/:versionId/probing/updateState",
-      async (req, res) => {
+      })
+      .get("/eservices/mainData/:eserviceRecordId", async (req, res) => {
         try {
-          await operationsService.updateEserviceProbingState(
-            req.params.eserviceId,
-            req.params.versionId,
-            req.body
-          );
-          return res.status(204).end();
-        } catch (error) {
-          const errorRes = makeApiProblem(error, updateEServiceErrorMapper);
-          return res.status(errorRes.status).json(errorRes).end();
-        }
-      }
-    )
-    .post(
-      "/eservices/:eserviceId/versions/:versionId/updateFrequency",
-      async (req, res) => {
-        try {
-          await operationsService.updateEserviceFrequency(
-            req.params.eserviceId,
-            req.params.versionId,
-            req.body
-          );
-          return res.status(204).end();
-        } catch (error) {
-          const errorRes = makeApiProblem(error, updateEServiceErrorMapper);
-          return res.status(errorRes.status).json(errorRes).end();
-        }
-      }
-    );
-
-  eServiceRouter
-    .get("/eservices", async (req, res) => {
-      try {
-        const { eserviceName, producerName, state, versionNumber } = req.query;
-        const eservices = await operationsService.getEservices(
-          {
-            ...(eserviceName && { eserviceName }),
-            ...(producerName && { producerName }),
-            ...(state && { state }),
-            ...(versionNumber && { versionNumber: Number(versionNumber) }),
-          },
-          req.query.limit,
-          req.query.offset
-        );
-
-        return res
-          .status(200)
-          .json({
-            content: eservices.content,
-            offset: eservices.offset,
-            limit: eservices.limit,
-            totalElements: eservices.totalElements,
-          })
-          .end();
-      } catch (error) {
-        const errorRes = makeApiProblem(error, () => 500);
-        return res.status(errorRes.status).json(errorRes).end();
-      }
-    })
-    .get("/eservices/mainData/:eserviceRecordId", async (req, res) => {
-      try {
-        const eServiceMainData = await operationsService.getEserviceMainData(
-          req.params.eserviceRecordId
-        );
-
-        return res.status(200).json(eServiceMainData).end();
-      } catch (error) {
-        const errorRes = makeApiProblem(error, updateEServiceErrorMapper);
-        return res.status(errorRes.status).json(errorRes).end();
-      }
-    })
-    .get("/eservices/probingData/:eserviceRecordId", async (req, res) => {
-      try {
-        const eServiceProbingData =
-          await operationsService.getEserviceProbingData(
+          const eServiceMainData = await operationsService.getEserviceMainData(
             req.params.eserviceRecordId
           );
 
-        return res.status(200).json(eServiceProbingData).end();
-      } catch (error) {
-        const errorRes = makeApiProblem(error, updateEServiceErrorMapper);
-        return res.status(errorRes.status).json(errorRes).end();
-      }
-    })
-    .get("/producers", async (req, res) => {
-      try {
-        const producers = await operationsService.getEservicesProducers(
-          {
-            producerName: req.query.producerName,
-          },
-          req.query.limit,
-          req.query.offset
-        );
+          return res.status(200).json(eServiceMainData).end();
+        } catch (error) {
+          const errorRes = resolveOperationsApiClientProblem(error);
+          return res.status(errorRes.status).json(errorRes).end();
+        }
+      })
+      .get("/eservices/probingData/:eserviceRecordId", async (req, res) => {
+        try {
+          const eServiceProbingData =
+            await operationsService.getEserviceProbingData(
+              req.params.eserviceRecordId
+            );
 
-        return res.status(200).json(producers).end();
-      } catch (error) {
-        const errorRes = makeApiProblem(error, () => 500);
-        return res.status(errorRes.status).json(errorRes).end();
-      }
-    });
+          return res.status(200).json(eServiceProbingData).end();
+        } catch (error) {
+          const errorRes = resolveOperationsApiClientProblem(error);
+          return res.status(errorRes.status).json(errorRes).end();
+        }
+      })
+      .get("/producers", async (req, res) => {
+        try {
+          const producers = await operationsService.getEservicesProducers(
+            {
+              producerName: req.query.producerName,
+            },
+            req.query.limit,
+            req.query.offset
+          );
 
-  return eServiceRouter;
+          return res.status(200).json(producers).end();
+        } catch (error) {
+          const errorRes = resolveOperationsApiClientProblem(error);
+          return res.status(errorRes.status).json(errorRes).end();
+        }
+      });
+
+    return router;
+  };
 };
 
 export default eServiceRouter;
