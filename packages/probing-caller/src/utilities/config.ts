f6e3f5bd6e756694b481a0aae2bc791214142e4b
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-02-28 08:37:11 +0100

    fix config

--- packages/probing-caller/src/utilities/config.ts
+++ packages/probing-caller/src/utilities/config.ts
@@ -5,7 +5,7 @@ import {
 } from "pagopa-interop-probing-commons";
 import { z } from "zod";
 
-const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig)
+const probingCallerConfig = AWSConfig.and(ConsumerConfig)
   .and(LoggerConfig)
   .and(
     z
@@ -15,9 +15,8 @@ const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig)
         SQS_ENDPOINT_POLL_RESULT_QUEUE: z.string(),
         SQS_ENDPOINT_TELEMETRY_RESULT_QUEUE: z.string(),
         AWS_REGION: z.string(),
-        CONSUMER_POLLING_TIMEOUT_IN_SECONDS: z.string(),
         LOG_LEVEL: z.string(),
-        JWT_PAYLOAD_EXPIRE_TIME_IN_SEC: z.string(),
+        JWT_PAYLOAD_EXPIRE_TIME_IN_SEC: z.coerce.number().min(1),
         JWT_PAYLOAD_ISSUER: z.string(),
         JWT_PAYLOAD_SUBJECT: z.string(),
         JWT_PAYLOAD_KID_KMS: z.string(),
@@ -28,7 +27,6 @@ const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig)
         sqsEndpointPollResultQueue: c.SQS_ENDPOINT_POLL_RESULT_QUEUE,
         sqsEndpointTelemetryResultQueue: c.SQS_ENDPOINT_TELEMETRY_RESULT_QUEUE,
         awsRegion: c.AWS_REGION,
-        consumerPollingTimeoutInSeconds: c.CONSUMER_POLLING_TIMEOUT_IN_SECONDS,
         logLevel: c.LOG_LEVEL,
         jwtPayloadExpireTimeInSec: c.JWT_PAYLOAD_EXPIRE_TIME_IN_SEC,
         jwtPayloadIssuer: c.JWT_PAYLOAD_ISSUER,
@@ -37,10 +35,10 @@ const eServiceResponseUpdaterConfig = AWSConfig.and(ConsumerConfig)
       }))
   );
 
-export type EServiceResponseUpdaterConfig = z.infer<
-  typeof eServiceResponseUpdaterConfig
+export type ProbingCallerConfig = z.infer<
+  typeof probingCallerConfig
 >;
 
-export const config: EServiceResponseUpdaterConfig = {
-  ...eServiceResponseUpdaterConfig.parse(process.env),
+export const config: ProbingCallerConfig = {
+  ...probingCallerConfig.parse(process.env),
 };
