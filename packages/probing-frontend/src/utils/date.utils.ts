7496c198de2db4641d81cfb92ab30e49a4101073
diego-dxc <diego.longo2@dxc.com>
2024-02-28 17:46:51 +0100

    removes try catch block

--- packages/probing-frontend/src/utils/date.utils.ts
+++ packages/probing-frontend/src/utils/date.utils.ts
@@ -1,11 +1,7 @@
 export function formatDateString(dateString: string) {
-  try {
-    const formatter = new Intl.DateTimeFormat('it', {
-      dateStyle: 'short',
-      timeStyle: 'short',
-    })
-    return formatter.format(new Date(dateString))
-  } catch {
-    return 'N/D'
-  }
+  const formatter = new Intl.DateTimeFormat('it', {
+    dateStyle: 'short',
+    timeStyle: 'short',
+  })
+  return formatter.format(new Date(dateString))
 }
