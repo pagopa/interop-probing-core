a927b2870104f9395c07c4bbea5d39841254f563
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-03-11 14:51:33 +0100

    Added workspace clients  with eservice-operations-client

---
+++ packages/clients/eservice-operations/src/types.ts
@@ -0,0 +1,52 @@
+import { ZodiosBodyByPath, ZodiosQueryParamsByPath } from "@zodios/core";
+import { api } from "./model/generated/client.js";
+
+export type Api = typeof api.api;
+
+export type ApiUpdateEserviceStatePayload = ZodiosBodyByPath<
+  Api,
+  "post",
+  "/eservices/:eserviceId/versions/:versionId/updateState"
+>;
+
+export type ApiUpdateEserviceProbingStatePayload = ZodiosBodyByPath<
+  Api,
+  "post",
+  "/eservices/:eserviceId/versions/:versionId/probing/updateState"
+>;
+
+export type ApiUpdateEserviceFrequencyPayload = ZodiosBodyByPath<
+  Api,
+  "post",
+  "/eservices/:eserviceId/versions/:versionId/updateFrequency"
+>;
+
+export type ApiSaveEservicePayload = ZodiosBodyByPath<
+  Api,
+  "put",
+  "/eservices/:eserviceId/versions/:versionId/saveEservice"
+>;
+
+export type ApiUpdateLastRequestPayload = ZodiosBodyByPath<
+  Api,
+  "post",
+  "/eservices/:eserviceRecordId/updateLastRequest"
+>;
+
+export type ApiUpdateResponseReceivedPayload = ZodiosBodyByPath<
+  Api,
+  "post",
+  "/eservices/:eserviceRecordId/updateResponseReceived"
+>;
+
+export type ApiSearchEservicesQuery = ZodiosQueryParamsByPath<
+  Api,
+  "get",
+  "/eservices"
+>;
+
+export type ApiGetProducersQuery = ZodiosQueryParamsByPath<
+  Api,
+  "get",
+  "/producers"
+>;
