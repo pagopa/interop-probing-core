196484efd90194afb65fc03d41a3aa04524ac722
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-03-20 15:14:44 +0100

    Update server configuration

--- packages/probing-api/src/app.ts
+++ packages/probing-api/src/app.ts
@@ -2,6 +2,9 @@ import { zodiosCtx } from "pagopa-interop-probing-commons";
 import { createApiClient } from "pagopa-interop-probing-eservice-operations-client";
 import eServiceRouter from "./routers/eserviceRouter.js";
 import { config } from "./utilities/config.js";
+import helmet from "helmet";
+import express from "express";
+import cors, { CorsOptions } from "cors";
 
 const operationsApiClient = createApiClient(config.operationsBaseUrl);
 
@@ -10,6 +13,43 @@ const app = zodiosCtx.app();
 // Disable the "X-Powered-By: Express" HTTP header for security reasons.
 // See https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#recommendation_16
 app.disable("x-powered-by");
+
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'none'"],
+        scriptSrc: ["'self'"],
+        connectSrc: ["'self'"],
+        imgSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        frameAncestors: ["'none'"],
+        formAction: ["'self'"],
+      },
+    })
+  );
+  
+  app.use(cors());
+  
+  app.use(
+    helmet.hsts({
+      includeSubDomains: true,
+      maxAge: 10886400,
+    })
+  );
+  
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+  app.use(helmet.frameguard({ action: "deny" }));
+  
+  const corsOptions: CorsOptions = {
+    origin: config.corsOriginAllowed,
+    methods: ["POST", "PUT", "GET", "OPTIONS", "DELETE"],
+    allowedHeaders: "*",
+  };
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors(corsOptions));
 
 app.use(eServiceRouter(zodiosCtx)(operationsApiClient));
 
