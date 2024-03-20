bdcda3496b675f5c016b00d7270a6dcc6581435f
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-03-20 15:31:29 +0100

    Fix lint

--- packages/probing-api/src/app.ts
+++ packages/probing-api/src/app.ts
@@ -52,7 +52,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
-app.use(healthRouter)
+app.use(healthRouter);
 app.use(eServiceRouter(zodiosCtx)(operationsApiClient));
 
 export default app;
