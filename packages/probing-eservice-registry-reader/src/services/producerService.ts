3a8bebb37d86cc331bdeb70e8fbb1d44f8322bf0
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-03-20 12:07:17 +0100

    Add probing eservice registry reader microservice

---
+++ packages/probing-eservice-registry-reader/src/services/producerService.ts
@@ -0,0 +1,17 @@
+import { SQS } from "pagopa-interop-probing-commons";
+import { EserviceDto } from "pagopa-interop-probing-models";
+import { config } from "../utilities/config.js";
+
+export const producerServiceBuilder = (sqsClient: SQS.SQSClient) => {
+  return {
+    async sendToServicesQueue(message: EserviceDto): Promise<void> {
+      await SQS.sendMessage(
+        sqsClient,
+        config.sqsEndpointServicesQueue,
+        JSON.stringify(message)
+      );
+    },
+  };
+};
+
+export type ProducerService = ReturnType<typeof producerServiceBuilder>;
