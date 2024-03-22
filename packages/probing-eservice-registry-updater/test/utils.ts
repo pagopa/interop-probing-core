7eee96dbbde71d87294edf8871e4abfbb0eb7d6e
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-03-22 12:56:47 +0100

    Add probing eservice registry updater microservice  (#19)

---
+++ packages/probing-eservice-registry-updater/test/utils.ts
@@ -0,0 +1,15 @@
+import { AxiosError, AxiosRequestHeaders } from "axios";
+
+export function mockApiClientError(status: number, statusText: string): AxiosError {
+  const mockAxiosError = new AxiosError(statusText);
+  mockAxiosError.response = {
+    status: status,
+    statusText: statusText,
+    headers: {},
+    config: {
+      headers: {} as AxiosRequestHeaders,
+    },
+    data: {},
+  };
+  return mockAxiosError;
+}
