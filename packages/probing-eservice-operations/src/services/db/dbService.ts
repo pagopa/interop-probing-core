f7c0bd9a74f580dbfc591abd6b9144991012197b
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-04-30 12:10:29 +0200

    Add comment ignore lint

--- packages/probing-eservice-operations/src/services/db/dbService.ts
+++ packages/probing-eservice-operations/src/services/db/dbService.ts
@@ -350,6 +350,7 @@ export function modelServiceBuilder(modelRepository: ModelRepository) {
       const result = z.array(EServiceContent).safeParse(
         data.map((d) => {
           return Object.fromEntries(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             Object.entries(d).filter(([_key, value]) => value !== null),
           );
         }),
