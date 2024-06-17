0eee5b00e1b7b396c91f3b034d87b0d96d321a39
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-17 21:30:35 +0200

    Sanitize EserviceDto basePath, audience properties

--- packages/models/src/queue/messages.ts
+++ packages/models/src/queue/messages.ts
@@ -57,10 +57,24 @@ export const EserviceDto = z.object({
   versionId: z.string().uuid(),
   technology: EserviceTechnology,
   state: EserviceInteropState,
-  basePath: z.array(z.string()).nonempty().max(2048),
+  basePath: z
+    .array(z.preprocess((val) => sanitizeData(val as string), z.string()))
+    .nonempty()
+    .max(2048),
   producerName: z.string().max(2048),
   versionNumber: z.number().int().min(1),
-  audience: z.array(z.string()).nonempty().max(2048),
+  audience: z
+    .array(z.preprocess((val) => sanitizeData(val as string), z.string()))
+    .nonempty()
+    .max(2048),
 });
 
 export type EserviceDto = z.infer<typeof EserviceDto>;
+
+/**
+ * Sanitizes the input string by removing control characters and trimming whitespace.
+ * Control characters removed include ASCII codes 0-31 and 127.
+ */
+const sanitizeData = (input: string): string => {
+  return input.replace(/[\x00-\x1F\x7F]+/g, "");
+};
