677d2f5bb6bc6de06dbb75f3c93b30a2739a20e0
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-06-17 10:03:49 +0200

    Update envs

--- packages/probing-eservice-operations/src/utilities/config.ts
+++ packages/probing-eservice-operations/src/utilities/config.ts
@@ -12,13 +12,13 @@ const eServiceOperationsConfig = HTTPServerConfig.and(LoggerConfig)
   .and(
     z
       .object({
-        APPLICATION_NAME: z.string(),
+        INTEROP_PROBING_OPERATIONS_APP_NAME: z.string(),
         TOLERANCE_MULTIPLIER_IN_MINUTES: z.coerce.number().min(1),
         POLLING_FREQUENCY_THRESHOLD: z.coerce.number().min(1),
         SCHEMA_NAME: z.string(),
       })
       .transform((c) => ({
-        applicationName: c.APPLICATION_NAME,
+        applicationName: c.INTEROP_PROBING_OPERATIONS_APP_NAME,
         minOfTolleranceMultiplier: c.TOLERANCE_MULTIPLIER_IN_MINUTES,
         pollingFrequencyThreshold: c.POLLING_FREQUENCY_THRESHOLD,
         schemaName: c.SCHEMA_NAME,
