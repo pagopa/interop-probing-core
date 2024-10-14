a30ff222a4e10fb4fa88f471fc5483cf305e1dc3
Eduardo Mihalache <eduardo.mihalache@dxc.com>
2024-10-14 16:36:39 +0200

    Update app probing-api (#77)

--- packages/probing-api/src/index.ts
+++ packages/probing-api/src/index.ts
@@ -2,6 +2,7 @@ import { genericLogger } from "pagopa-interop-probing-commons";
 import { config } from "./utilities/config.js";
 import app from "./app.js";
 
+// App
 app.listen(config.port, config.host, () => {
   genericLogger.info(`listening on ${config.host}:${config.port}`);
 });
