2bdac770baf81d4db902a0f8bfb3ed7cc0074f9e
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-10-29 01:20:17 +0100

    Removed duplicated RESTS

--- packages/probing-eservice-operations/src/routers/eserviceRouter.ts
+++ packages/probing-eservice-operations/src/routers/eserviceRouter.ts
@@ -42,23 +42,6 @@ const eServiceRouter = (
       },
     )
     .post(
-      "/eservices/:eserviceId/versions/:versionId/probing/updateState",
-      async (req, res) => {
-        try {
-          await eServiceService.updateEserviceProbingState(
-            req.params.eserviceId,
-            req.params.versionId,
-            req.body,
-            logger(req.ctx),
-          );
-          return res.status(204).end();
-        } catch (error) {
-          const errorRes = makeApiProblem(error, errorMapper, logger(req.ctx));
-          return res.status(errorRes.status).json(errorRes).end();
-        }
-      },
-    )
-    .post(
       "/eservices/:eserviceId/versions/:versionId/updateFrequency",
       async (req, res) => {
         try {
@@ -80,23 +63,6 @@ const eServiceRouter = (
       async (req, res) => {
         try {
           await eServiceService.updateEserviceProbingState(
-            req.params.eserviceId,
-            req.params.versionId,
-            req.body,
-            logger(req.ctx),
-          );
-          return res.status(204).end();
-        } catch (error) {
-          const errorRes = makeApiProblem(error, errorMapper, logger(req.ctx));
-          return res.status(errorRes.status).json(errorRes).end();
-        }
-      },
-    )
-    .post(
-      "/eservices/:eserviceId/versions/:versionId/updateFrequency",
-      async (req, res) => {
-        try {
-          await eServiceService.updateEserviceFrequency(
             req.params.eserviceId,
             req.params.versionId,
             req.body,
