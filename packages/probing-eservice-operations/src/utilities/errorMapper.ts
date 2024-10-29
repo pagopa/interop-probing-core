9107d679caba78299442e8d0cac4818cb5a112d0
Eduardo Mihalache <eduardo.mihalache@mobilesoft.it>
2024-10-29 08:42:08 +0100

    Added tenantNotFound to httpErrorMapper

--- packages/probing-eservice-operations/src/utilities/errorMapper.ts
+++ packages/probing-eservice-operations/src/utilities/errorMapper.ts
@@ -10,4 +10,5 @@ const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = constants;
 export const errorMapper = (error: ApiError<ErrorCodes>): number =>
   match(error.code)
     .with("eServiceNotFound", () => HTTP_STATUS_NOT_FOUND)
+    .with("tenantNotFound", () => HTTP_STATUS_NOT_FOUND)
     .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
