37528041618ac0a1be074528c12b8a90c6954f48
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2025-11-17 18:13:17 +0100

    chore: removed unused ErrorCodes

--- packages/probing-caller/src/model/domain/errors.ts
+++ packages/probing-caller/src/model/domain/errors.ts
@@ -1,27 +1,10 @@
-import { ZodError } from "zod";
 import { InternalError } from "pagopa-interop-probing-models";
 
 export const errorCodes = {
-  callProbingEndpointError: "CALL_PROBING_ENDPOINT_ERROR",
   buildJWTError: "BUILD_JWT_ERROR",
 } as const;
 
 export type ErrorCodes = keyof typeof errorCodes;
-
-export function callProbingEndpointError(
-  detail: string,
-  error: unknown,
-): InternalError<ErrorCodes> {
-  const zodiosErrorCause = (error as ZodError)?.cause;
-  const zodiosErrorDetails: string = zodiosErrorCause
-    ? `Cause: ${JSON.stringify(zodiosErrorCause)}`
-    : "";
-
-  return new InternalError({
-    detail: `${detail} ${zodiosErrorDetails}`,
-    code: "callProbingEndpointError",
-  });
-}
 
 export function buildJWTError(detail: string): InternalError<ErrorCodes> {
   return new InternalError({
