3d0cea0db7d3e3eae274f3c6dcd57ff059a52fd0
Eduardo Mihalache <eduardo.mihalache@digitouch.it>
2025-12-02 08:41:51 +0100

    PIN-8693: add date range validation filtered telemetryData (#132)

--- packages/probing-statistics-api/src/utilities/errorMapper.ts
+++ packages/probing-statistics-api/src/utilities/errorMapper.ts
@@ -1,10 +1,14 @@
 import { constants } from "http2";
 import { ApiError, CommonErrorCodes } from "pagopa-interop-probing-models";
 import { match } from "ts-pattern";
+import { ErrorCodes as LocalErrorCodes } from "../model/domain/errors.js";
 
-type ErrorCodes = CommonErrorCodes;
+type ErrorCodes = LocalErrorCodes | CommonErrorCodes;
 
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = constants;
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_BAD_REQUEST } =
+  constants;
 
 export const errorMapper = (error: ApiError<ErrorCodes>): number =>
-  match(error.code).otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
+  match(error.code)
+    .with("invalidFilterDate", () => HTTP_STATUS_BAD_REQUEST)
+    .otherwise(() => HTTP_STATUS_INTERNAL_SERVER_ERROR);
