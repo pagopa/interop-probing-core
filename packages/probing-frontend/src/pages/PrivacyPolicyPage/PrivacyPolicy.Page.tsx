f7de000d9557d0d0b37023817c2915ee6195a411
ruggero.castagnola <ruggerocastagnola@gmail.com>
2026-06-16 11:24:23 +0200

    Fixed privacy and legal pages

--- packages/probing-frontend/src/pages/PrivacyPolicyPage/PrivacyPolicy.Page.tsx
+++ packages/probing-frontend/src/pages/PrivacyPolicyPage/PrivacyPolicy.Page.tsx
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+const extendedWindow: Window & { OneTrust?: any } = window
 
 export const PrivacyPolicyPage: React.FC = () => {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `<!-- OneTrust Privacy Notice start -->
-        <!-- Container in which the Privacy Notice will be rendered -->
-        <div id="otnotice-673b25bc-120d-4e35-b677-372c40a01a93" class="otnotice"></div>
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.id = 'otprivacy-notice-script'
+    script.src =
+      'https://privacyportalde-cdn.onetrust.com/privacy-notice-scripts/otnotice-1.0.min.js'
+    script.type = 'text/javascript'
+    script.charset = 'UTF-8'
+    script.setAttribute(
+      'settings',
+      'eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0='
+    )
 
-        <script src="https://privacyportalde-cdn.onetrust.com/privacy-notice-scripts/otnotice-1.0.min.js" type="text/javascript" charset="UTF-8" id="otprivacy-notice-script">
-          settings="eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtZGUub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1kZS5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0="
-        </script>
+    script.onload = () => {
+      if (extendedWindow.OneTrust?.NoticeApi) {
+        extendedWindow.OneTrust.NoticeApi.Initialized.then(() => {
+          extendedWindow.OneTrust.NoticeApi.LoadNotices([
+            'https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/ae5b9e56-091c-421a-9323-6029e9ea9076/published/privacynotice.json',
+          ])
+        })
+      }
+    }
 
-        <script type="text/javascript" charset="UTF-8">
-          // To ensure external settings are loaded, use the Initialized promise:
-          OneTrust.NoticeApi.Initialized.then(function() {
-            OneTrust.NoticeApi.LoadNotices(["https://privacyportalde-cdn.onetrust.com/storage-container/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/673b25bc-120d-4e35-b677-372c40a01a93/published/privacynotice.json"]);
-          });
-        </script>
-        <!-- OneTrust Privacy Notice end -->`,
-      }}
-    ></div>
-  )
+    document.head.appendChild(script)
+
+    return () => {
+      const el = document.getElementById('otprivacy-notice-script')
+      if (el) el.remove()
+    }
+  }, [])
+
+  return <div id="otnotice-ae5b9e56-091c-421a-9323-6029e9ea9076" className="otnotice"></div>
 }
